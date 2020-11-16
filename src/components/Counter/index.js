69c20cbe4a70ab178af4d94b3564001c9cdcaa50
Kim Thompson <kimthompson@hey.com>
2020-11-16 16:18:01 -0600

    docz as a note taking/code exercise platform . . . im so smrt

---
+++ src/components/Counter/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import { render } from 'react-dom';
+
+import './styles.css';
+
+class Counter extends Component {
+  render() {
+    return (
+      <main className="Counter">
+        <p className="count">0</p>
+        <section className="controls">
+          <button>Increment</button>
+          <button>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Counter;
