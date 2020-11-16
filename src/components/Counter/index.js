37ea68164e92b441179eac0665121886b7884bbe
Kim Thompson <kimthompson@hey.com>
2020-11-16 16:42:39 -0600

    solution 1

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,21 +1,83 @@
-import React, { Component } from 'react';
-//import { render } from 'react-dom';
+import './styles.css'
 
-import './styles.css';
+import React from 'react'
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
 
-class Counter extends Component {
-  render() {
-    return (
-      <main className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </main>
-    );
-  }
+const initialState = {
+  count: 0
 }
 
-export default Counter;
+// seems unnecessary, but will blow up if you typo instead of failing silently
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const increment = () => ({
+  type: INCREMENT
+})
+
+const decrement = () => ({
+  type: DECREMENT
+})
+
+const reset = () => ({
+  type: RESET
+})
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      }
+    case DECREMENT:
+      return {
+        count: state.count - 1
+      }
+    case RESET:
+      return {
+        count: 0
+      }
+  }
+
+  return state
+}
+
+const store = createStore(reducer)
+
+const Counter = ({ count, increment, decrement, reset }) => {
+  console.log({ count, increment })
+
+  return (
+    <main className="Counter">
+      <p className="count">{ count }</p>
+      <section className="controls">
+        <button onClick={ increment }>Increment</button>
+        <button onClick={ decrement }>Decrement</button>
+        <button onClick={ reset }>Reset</button>
+      </section>
+    </main>
+  )
+}
+
+// We want to only pass in the props it cares about to as to not trigger rerenders too often
+// Right now that's fine b/c app is smol and state is teeny, later that'll be undesirable
+const mapStateToProps = state => { return state }
+const mapDispatchToProps = {
+  increment,
+  decrement,
+  reset
+}
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const HOC = () => {
+  return (
+    <Provider store={store}>
+      <CounterContainer />
+    </Provider>
+  )
+}
+
+export default HOC
