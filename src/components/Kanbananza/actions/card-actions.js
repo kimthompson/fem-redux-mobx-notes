695fb6abee4c9313d677218e180c3150013e1dd1
Kim Thompson <kimthompson@hey.com>
2020-11-17 20:25:30 -0600

    most of exercise 2 done?

---
+++ src/components/Kanbananza/actions/card-actions.js
@@ -0,0 +1,21 @@
+const CARD_CREATE = 'CARD_CREATE';
+
+const defaultCardData = {
+  title: '',
+  description: '',
+  assignedTo: ''
+}
+
+export const createCard = (listId, cardData) => {
+  const cardId = Date.now().toString()
+  const card = {
+    id: cardId,
+    ...defaultCardData,
+    ...cardData // overrides
+  }
+
+  return {
+    type: CARD_CREATE,
+    payload: { card, listId, cardId }
+  }
+}
